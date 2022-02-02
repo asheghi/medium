95358517dda7c83e85e960db015195a6d2245411
SyedHassan Asheghi <Asheghi.sh@gmail.com>
2022-02-03 02:20:11 +0330

    home page progress

--- pages/index/home-index.page.server.js
+++ pages/index/home-index.page.server.js
@@ -1,5 +1,4 @@
 import { stringify, parse } from 'qs';
-import { marked } from 'marked';
 import { ax } from '../../lib/plugins/axios';
 import { postsPerPage } from '../../lib/config';
 import { getDebug, parseAxiosError } from '../../lib/utils';
@@ -10,10 +9,11 @@ export const passToClient = ['pageProps', 'urlPathname', 'posts', 'pagination'];
 export async function onBeforeRender(pageContext) {
   const { url } = pageContext;
   const queryParams = parse(url.substring(url.indexOf('?') + 1));
-  const page = queryParams.page || 1;
+  const page = queryParams && queryParams.page || 1;
   const extra = {};
   const query = stringify({
     sort: ['publishedAt:desc'],
+    populate: '*',
     pagination: {
       pageSize: postsPerPage,
       page,
@@ -24,8 +24,10 @@ export async function onBeforeRender(pageContext) {
   try {
     const { data, status } = await ax.get(`posts?${query}`);
     if (status === 200) {
-      extra.posts = data.data.map((it) => ({ ...it.attributes, id: it.id }))
-        .map((it) => ({ ...it, content: marked.parse(it.content || '') }));
+      extra.posts = data.data.map((it) => ({
+        ...it.attributes,
+        id: it.id,
+      }));
       extra.pagination = data.meta.pagination;
     } else {
       debug('api response status is not ok!', status, data);
